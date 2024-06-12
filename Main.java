54d1458111c18aef604e95b4a5d2b3380c527a4a
Elber Gutierrez <gutiere5@ferris.edu>
2024-06-12 13:27:25 -0400

    Added code to get input from user using Scanner

--- Main.java
+++ Main.java
@@ -1,1 +1,23 @@
-// Testing
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner scnr = new Scanner(System.in);
+		// TODO Auto-generated method stub
+		
+		// Just adding a comment for git - for real
+		
+		// Making another commit
+				
+		System.out.println("Type something: ");
+		String input = scnr.next();
+		
+		
+		
+		
+		System.out.println(input);
+
+	}
+
+}
